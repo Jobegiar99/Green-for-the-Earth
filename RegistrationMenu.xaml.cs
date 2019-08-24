f9d7798f8b64e52f8285a2e1740aff8d33ebe889
Bernardo Arana <bernardo.giron99@gmail.com>
2019-08-24 16:54:41 -0500

    Main Menu and Registration Menu are done. Will start working on user menu.

--- RegistrationMenu.xaml.cs
+++ RegistrationMenu.xaml.cs
@@ -11,28 +11,25 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Green_for_the_Earth
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class RegistrationMenu : Page
     {
-        public MainPage()
-        {    
+        public RegistrationMenu()
+        {
             this.InitializeComponent();
-            
-            
         }
 
-        private void Button_register_Click(object sender, RoutedEventArgs e)
+        private void Button_Return_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(RegistrationMenu));
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
