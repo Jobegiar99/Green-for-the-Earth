a773bdfc510419de523cce769e152d64fbcdd0f4
Ian Andre Aragon Saenz <ian.aragon@axen.pro>
2019-08-24 22:55:30 -0500

    made database

---
+++ Data/GreenContext.cs
@@ -0,0 +1,21 @@
+﻿using Green_for_the_Earth.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_for_the_Earth.Data
+{
+    public class GreenContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Earth.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
