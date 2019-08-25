9ab551e58d80e563e15e540a09db5a1490abba97
Ian Andre Aragon Saenz <ian.aragon@axen.pro>
2019-08-24 20:35:20 -0500

    Made database

---
+++ Migrations/20190825013418_initial.Designer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Green_for_the_Earth.Data;
+
+namespace Green_for_the_Earth.Migrations
+{
+    [DbContext(typeof(GreenContext))]
+    [Migration("20190825013418_initial")]
+    partial class initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.6");
+
+            modelBuilder.Entity("Green_for_the_Earth.Model.User", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Password");
+
+                    b.Property<string>("UserName");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Users");
+                });
+        }
+    }
+}
